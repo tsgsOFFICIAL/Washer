85221e22a94d1a46e251d4eb40bbaaadc4232594
Marcus Jensen <marcusproman0507@gmail.com>
2020-08-24 14:09:47 +0200

    Add project files.

---
+++ Washer/Program.cs
@@ -0,0 +1,100 @@
+﻿#region Imports / Usings
+using System;
+#endregion
+
+namespace Washer
+    {
+    class Program
+        {
+        /// <summary>
+        /// Main method, start here!
+        /// </summary>
+        /// <param name="args">Passed arguments will be treated as parameters under Main method</param>
+        static void Main(string[] args)
+            {
+            Console.Title = "EZ Washer by tsgs"; //Change the title
+            ReadAsciiMessage(@"Messages/WelcomeMsg.tsgs"); //Read and display ascii welcome message
+
+            System.Threading.Thread.Sleep(750); //Wait for 0.75 seconds
+
+            Washer myWasher = new Washer(); //Initialize/Generate a new instance of a Washer, called myWasher
+
+            bool runLoop = true;
+            while (runLoop)
+                {
+                Display(myWasher); //Update the display
+                switch (Console.ReadKey(true).KeyChar)
+                    {
+                    case '1':
+                        myWasher.TogglePower();
+                        break;
+                    case '2':
+                        if (myWasher.RunningProgram == "nothing")
+                            {
+                            Console.WriteLine("What program do you want to run?\nYour options are:\n");
+                            foreach (string program in Enum.GetNames(typeof(Washer.Program)))
+                                {
+                                Console.WriteLine("\"{0}\"", program);
+                                }
+                            Console.Write("Program: ");
+
+                            if (!myWasher.Start(Console.ReadLine()))
+                                {
+                                Console.WriteLine("Make sure you plug the machine in, and check your spelling!");
+                                System.Threading.Thread.Sleep(500);
+                                }
+                            }
+                        else
+                            {
+                            Console.WriteLine("Already running {0}", myWasher.RunningProgram);
+                            System.Threading.Thread.Sleep(500);
+                            }
+                        break;
+                    case '3':
+                        myWasher.Stop();
+                        break;
+                    case '4':
+                        Console.Clear(); //Clear the console window
+                        runLoop = false;
+                        break;
+                    }
+                }
+
+
+            //Exit
+            ReadAsciiMessage(@"Messages/ExitMsg.tsgs");
+            Console.ReadKey(true);
+            Environment.Exit(0); //Exit with error code '0'
+            }
+
+        /// <summary>
+        /// Read Ascii Message 
+        /// </summary>
+        /// <param name="filePath"></param>
+        private static void ReadAsciiMessage(string filePath)
+            {
+            string[] msg = System.IO.File.ReadAllLines(filePath);
+            foreach (string line in msg)
+                {
+                Console.WriteLine(line);
+                }
+            }
+
+        private static void Display(Washer washer)
+            {
+            Console.Clear();
+            Console.WriteLine("Controls:");
+            Console.WriteLine("Press (1) to toggle power");
+            Console.WriteLine("Press (2) to toggle the centrifuge running");
+            Console.WriteLine("Press (3) to stop the centrifuge");
+            Console.WriteLine("Press (4) to exit\n");
+            Console.WriteLine("Power state is {0}", washer.PowerState);
+            Console.WriteLine("Centrifuge state is {0}", washer.CentrifugeState);
+            Console.WriteLine("Current program is {0}", washer.RunningProgram);
+            Console.WriteLine("Temperature is {0}\u00B0", washer.Temperature);
+            Console.WriteLine("Weight is {0} kg", washer.Weight);
+            Console.WriteLine("The door is {0}", washer.DoorState);
+            }
+
+        }
+    }
---
+++ Washer/Washer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Washer
+    {
+    class Washer
+        {
+        #region Fields
+        private bool powerState; //powerState refers to wether the washer has power or not
+        private bool runState; //runState refers to the state of the washer, is it running or not
+        private bool isWaterConnected; //Is the water properly connected
+        private bool isDoorOpen; //Is the door open
+        private short weight; //Weight in kg's
+        private short temperature; //temp refers to the temperature of the water inside the washer
+        private string runningProgram; //Holds the running program
+        /// <summary>
+        /// A list of available programs for your washer
+        /// </summary>
+        public enum Program
+            {
+            eco,
+            quick
+            }
+        #endregion
+
+        #region Properties
+        public string PowerState
+            {
+            get { if (this.powerState) { return "on"; } else { return "off"; } }
+            }
+        public string CentrifugeState
+            {
+            get { if (this.runState) { return "on"; } else { return "off"; } }
+            }
+        public string RunningProgram
+            {
+            get { if (this.runningProgram == null || this.runningProgram == "") { return "nothing"; } else { return this.runningProgram; } }
+            }
+        public string DoorState
+            {
+            get { if (this.isDoorOpen) { return "open"; } else { return "closed"; } }
+            }
+        public string Temperature
+            {
+            get { return temperature.ToString(); }
+            }
+        public string Weight
+            {
+            get { return weight.ToString(); }
+            }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Powers on the washing machine, doesn't start it!
+        /// </summary>
+        private void PowerOn()
+            {
+            powerState = true;
+            }
+
+        /// <summary>
+        /// Powers off the washing machine, ending everything it might be doing
+        /// </summary>
+        private void PowerOff()
+            {
+            if (runState)
+                {
+                runState = false;
+                runningProgram = "";
+                }
+            powerState = false;
+            }
+
+        /// <summary>
+        /// Toggle the power on or off, depending on the current state
+        /// </summary>
+        public void TogglePower()
+            {
+            if (powerState)
+                {
+                PowerOff();
+                }
+            else
+                {
+                PowerOn();
+                }
+            }
+
+        /// <summary>
+        /// Start the washer at a certain program
+        /// </summary>
+        /// <param name="program">Choose what program to run</param>
+        /// <returns>This method returns true if successful, otherwise returns false</returns>
+        public bool Start(string program)
+            {
+            if (this.powerState)
+                {
+                switch (program.ToLower().Trim())
+                    {
+                    case "eco":
+                        break;
+                    case "quick":
+                        break;
+                    default:
+                        return false;
+                    }
+                runningProgram = program.ToLower().Trim();
+
+                runState = true;
+                return true;
+                }
+            else
+                {
+                return false;
+                }
+            }
+
+        /// <summary>
+        /// Stops the running program, if any.
+        /// </summary>
+        public void Stop()
+            {
+            runState = false;
+            runningProgram = "";
+            }
+        #endregion
+        }
+    }
